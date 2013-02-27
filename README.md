182fa5fcf216ee94719ee6200a44549e9038b431
Grant Bacon <btnarg@gmail.com>
2013-02-27 11:09:00 -0600

    Use configuration file

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ A Web-Controlled Speech Robot written with the Raspberry Pi in mind
 This project is a learning experience for me. I've got a Raspberry Pi hooked up to a set of speakers in my living room. Using this website people can communicate through the web interface to send messages to me and my roommates.
 
 The web interface uses bottle.py to handle routing and requests. It's been great working with bottle.py, very simple.
+
+
+_Required_
+bottle.py
+config module (use easy_install)
--- web_ui.py
+++ web_ui.py
@@ -1,12 +1,15 @@
 from poebot import Poebot
 from bottle import run, route, request, template, static_file
+from config import Config
 import json
 
-poe = Poebot('log.html')
+cfg = Config('poebot.cfg')
+
+poe = Poebot(cfg.log_file)
 
 @route('/static/<filepath:path>')
 def static(filepath):
-	return static_file(filepath, root='/home/pi/poebot/static')
+	return static_file(filepath, root=cfg.static_dir)
 
 @route('/speak', method='POST')
 def speak():
@@ -22,4 +25,4 @@ def about():
 def index():
     return template('index')
 
-run(host='192.168.1.222', port=8080)
+run(host=cfg.addr, port=cfg.port)
